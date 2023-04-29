2219b1246a016c8e1caa055875a0ffd3261ef3c2
Evgeniy <76659477+evdevru@users.noreply.github.com>
2023-04-29 11:24:55 +0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { anu } from 'anu-vue'
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// UnoCSS import
+import 'uno.css'
+
+// import styles
+import 'anu-vue/dist/style.css'
+
+// default theme styles
+import '@anu-vue/preset-theme-default/dist/style.css'
+
+createApp(App)
+    .use(anu, { registerComponents: true })
+    .mount('#app')
